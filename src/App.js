1861ab90c7e476d4efd289f1d137bcb133546e11
Nishad Islam <nishad.bdg@gmail.com>
2020-12-04 10:32:07 +0600

    components and containers added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Home from './containers/Home'
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <Home></Home>
     </div>
   );
 }
---
+++ src/containers/Home/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './style.css'
+
+const Home = props => {
+    return (
+        <div>Home</div>  
+    );
+}
+
+export default Home;
