b4b3dbe9e62fd57d6b714264cd7647a9913eeed8
Nishad Islam <nishad.bdg@gmail.com>
2020-12-05 12:02:51 +0600

    search animation added

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './style.css';
+import searchImg from '../../assets/icons/search.png'
 
 const Navbar = (props) =>{
+
+    const [search, setSearch] = useState(false);
+
+    const submitSearch = (e) =>{
+        e.preventDefault();
+        alert("Searched");
+    }
+
+    const openSearch = () =>{
+        setSearch(true);
+    }
+
+    const searchClass = search ? 'searchInput active' : 'searchInput' ;
+
     return(
         <div className="navbar">
             <ul className="navbarMenu">
@@ -9,6 +24,12 @@ const Navbar = (props) =>{
                 <li><a href="#">About Us</a></li>
                 <li><a href="#">Contact Us</a></li>
             </ul>
+            <div className="search">
+               <form onSubmit= {submitSearch}>
+               <input type='text' className={searchClass} placeholder="Search" />
+                <img onClick={openSearch} className="searchIcon" src= {searchImg} />
+               </form>
+            </div>
         </div>
     );
 }
